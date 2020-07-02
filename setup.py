9f9cfd7b48604e43d1207835b6b227a22e098687
Juan Pablo <juanpablomartinez590@hotmail.com>
2020-07-02 12:15:34 +0200

    Project structure

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+with open('README.md', encoding='UTF-8') as readme_file:
+    readme = readme_file.read()
+
+
+setup(
+    name='pg-backup',
+    version='0.1.0',
+    description='Postgres database backups locally or to AWS S3.',
+    long_description=readme,
+
+    author='Juan Pablo Aguirre',
+    author_email='juanpablomartinez590@hotmail.com',
+
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+
+    install_requires=[]
+)
